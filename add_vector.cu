03333e246457df45bd5fa7a848688dad4796b060
NguyenThaiVu <vunguyenthai73@gmail.com>
2025-08-30 08:23:50 +0700

    hot fix code

--- add_vector.cu
+++ add_vector.cu
@@ -1,127 +1,74 @@
 #include <iostream>
 using namespace std;
 #include <stdio.h>
+#include </home/thaiv7/Desktop/cuda_project/utils/in_out_helper.h>
+#include </home/thaiv7/Desktop/cuda_project/utils/gpu_helper.cu>
 
-#define CHECK(call)\
-{\
-    const cudaError_t error = call;\
-    if (error != cudaSuccess)\
-    {\
-        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
-        fprintf(stderr, "code: %d, reason: %s\n", error,\
-                cudaGetErrorString(error));\
-        exit(1);\
-    }\
-}
-
-struct GpuTimer
-{
-    cudaEvent_t start;
-    cudaEvent_t stop;
-
-    GpuTimer()
-    {
-        cudaEventCreate(&start);
-        cudaEventCreate(&stop);
-    }
-
-    ~GpuTimer()
-    {
-        cudaEventDestroy(start);
-        cudaEventDestroy(stop);
-    }
-
-    void Start()
-    {
-        cudaEventRecord(start, 0);
-        cudaEventSynchronize(start);
-    }
-
-    void Stop()
-    {
-        cudaEventRecord(stop, 0);
-    }
-
-    float Elapsed()
-    {
-        float elapsed;
-        cudaEventSynchronize(stop);
-        cudaEventElapsedTime(&elapsed, start, stop);
-        return elapsed;
-    }
-};
-
-
-void printDeviceInfo() 
-{
-    cudaDeviceProp devProv;
-    CHECK(cudaGetDeviceProperties(&devProv, 0));
-    printf("**********GPU info**********\n");
-    printf("Name: %s\n", devProv.name);
-    printf("Compute capability: %d.%d\n", devProv.major, devProv.minor);
-    printf("Num SMs: %d\n", devProv.multiProcessorCount);
-    printf("Max num threads per SM: %d\n", devProv.maxThreadsPerMultiProcessor); 
-    printf("Max num warps per SM: %d\n", devProv.maxThreadsPerMultiProcessor / devProv.warpSize);
-    printf("GMEM: %zu byte\n", devProv.totalGlobalMem);
-    printf("SMEM per SM: %zu byte\n", devProv.sharedMemPerMultiprocessor);
-    printf("SMEM per block: %zu byte\n", devProv.sharedMemPerBlock);
-    printf("****************************\n");
-}
-
-
-__global__ void AddTwoVectors(float A[], float B[], float C[], int N) 
+__global__ void addTwoVectorsDevice(float A[], float B[], float C[], int N)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     C[i] = A[i] + B[i];
 }
 
-int main(void) 
+void addTwoVectorHost(float A[], float B[], float C[], int N)
 {
-    int N = 10000;
-    float A[N], B[N], C[N]; // Arrays for vectors A, B, and C
-
-    for (int i = 0; i < N; ++i) {
-        A[i] = i;
-        B[i] = i+1;
+    for (int i = 0; i < N; i++)
+    {
+        C[i] = A[i] + B[i];
     }
+}
 
-    // Device pointers for vectors A, B, and C
-    float *d_A, *d_B, *d_C; 
+int main(void)
+{
+    int N = 1000000;
+    float *A = new float[N];
+    float *B = new float[N];
+    float *C = new float[N];
+    float *trueC = new float[N];
 
-    cudaMalloc((void **)&d_A, N * sizeof(float));
-    cudaMalloc((void **)&d_B, N * sizeof(float));
-    cudaMalloc((void **)&d_C, N * sizeof(float));
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = i;
+        B[i] = i;
+    }
+    addTwoVectorHost(A, B, trueC, N);
 
-    // Copy data from host to device
-    cudaMemcpy(d_A, A, N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, N * sizeof(float), cudaMemcpyHostToDevice);    
+    float *d_A, *d_B, *d_C;
+    cudaMalloc((void**)&d_A, N * sizeof(float));
+    cudaMalloc((void**)&d_B, N * sizeof(float));
+    cudaMalloc((void**)&d_C, N * sizeof(float));
 
-    int BLOCK_SIZE = 256;
-    int GRID_SIZE = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    cudaMemcpy(d_A, A, N*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, N*sizeof(float), cudaMemcpyHostToDevice);
+
+    int BLOCK_SIZE = 32;
+    int NUM_GRID = int(N / BLOCK_SIZE) + 1;
 
     GpuTimer timer;
     timer.Start();
-    AddTwoVectors<<<GRID_SIZE, BLOCK_SIZE>>>(d_A, d_B, d_C, N);
-    cudaDeviceSynchronize();
+    addTwoVectorsDevice<<<NUM_GRID, BLOCK_SIZE>>>(d_A, d_B, d_C, N);
+    cudaDeviceSynchronize(); // wait to device computation is finished.
     timer.Stop();
-    printf("Time: %.3f ms\n", timer.Elapsed());
+    printf("GPU Time: %.3f ms\n", timer.Elapsed());
 
-    // Copy data from device to host
     cudaMemcpy(C, d_C, N * sizeof(float), cudaMemcpyDeviceToHost);
-
-    // Check for error
     cudaError_t error = cudaGetLastError();
     if(error != cudaSuccess) {
         printf("CUDA error: %s\n", cudaGetErrorString(error));
         exit(-1);
     }
-    
-    // for (int i = 0; i < N; ++i) {
-    //     printf("C[%d] = %f\n", i, C[i]);
-    // }
+
+    bool isSimilar = compare2Vector(C, trueC, N);
+    if (isSimilar == true)
+    {
+        cout << "CUDA implementation is correct" << endl;
+    } else {
+        cout << "CUDA is incorrect" << endl;
+    }
 
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
+
     return 0;
 }
--- utils/in_out_helper.h
+++ utils/in_out_helper.h
@@ -4,36 +4,60 @@
 #include <fstream>
 using namespace std;
 
+void print1DArray(float *A, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+}
 
+bool compare2Vector(float *A, float *B, int N)
+{
+    bool isSimilar = true;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] != B[i])
+        {
+            isSimilar = false;
+            break;
+        }
+    }
+    return isSimilar;
+}
 
 // Function to load weights/bias from a .txt file
-void load_weights(const string& filename, float* weight, int size) 
+void load_weights(const string &filename, float *weight, int size)
 {
     ifstream infile(filename);
 
-    if (infile.is_open()) {
-        for (int i = 0; i < size; ++i) {
+    if (infile.is_open())
+    {
+        for (int i = 0; i < size; ++i)
+        {
             infile >> weight[i];
         }
-    } else {
+    }
+    else
+    {
         std::cerr << "Could not open file: " << filename << std::endl;
     }
 
     infile.close();
 }
 
-
-
-
 void save_to_file(float x[], int size, string filename)
 {
-    ofstream myfile (filename);
+    ofstream myfile(filename);
     if (myfile.is_open())
     {
-        for(int count = 0; count < size; count ++){
-            myfile << x[count] << endl ;
+        for (int count = 0; count < size; count++)
+        {
+            myfile << x[count] << endl;
         }
         myfile.close();
     }
-    else cout << "Unable to open file";
+    else
+        cout << "Unable to open file";
 }
